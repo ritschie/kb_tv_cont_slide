6ebd56d52c58a8446f87532739df54d2d36e4f80
Stefan Rotsch <stefan.rotsch@aoemedia.de>
2013-08-27 12:29:07 +0200

    [TASK] Import official version from TER

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "kb_tv_cont_slide".
+#
+# Auto generated 25-03-2012 18:30
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'KB TV Content Slide',
+	'description' => 'This extension lets you insert the content of a TV Column field into a TV TS-Object path field. But if no content is found in the column on the actual page the content of the parent page is taken. Other description: The content of the root-page is inherited to its child pages',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '0.4.4',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '4.0.0-0.0.0',
+	'PHP_version' => '4.0.0-0.0.0',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Bernhard Kraft',
+	'author_email' => 'kraftb@kraftb.at',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+);
+
+?>
